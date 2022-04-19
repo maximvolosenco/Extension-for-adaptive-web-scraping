5750260099283a97bbf75ac1255e952ba9274203
mvolosen <maximvolosenco@gmail.com>
2022-04-19 19:27:38 +0300

    [feat] set up entity framework for asp.net api

--- src/Adaptive.WebApi/Program.cs
+++ src/Adaptive.WebApi/Program.cs
@@ -1,3 +1,9 @@
+using Adaptive.Data;
+using Adaptive.Data.DbInitializer;
+using Adaptive.Data.Repository;
+using Adaptive.Data.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure EF core  
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddDbContext<AdaptiveContext>(options => options.UseSqlServer("Data Source=.;Initial Catalog=AdaptiveDb;Integrated Security=True"));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+
 
 var app = builder.Build();
 
@@ -16,7 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
