4d5ec89b1de17db7b52764b3e9b6dd652f0dde80
mvolosen <maximvolosenco@gmail.com>
2022-04-19 16:39:05 +0300

    [feat] create base entity for EF core and User entity

---
+++ src/Adaptive.DataObjects/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace Adaptive.DataObjects
+{
+    public class Entity
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ src/Adaptive.DataObjects/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Adaptive.DataObjects
+{
+    public class User
+    {
+        [MaxLength(250)]
+        public string Email { get; set; }
+    }
+}
